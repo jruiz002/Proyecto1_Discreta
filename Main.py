941d1a7b684447b777c9d6d9d338222e5dc43d8e
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-08-10 23:08:35 -0600

    Implementación de tokenización y procesamiento de operaciones entre conjuntos utilizando la estructura de datos de pilas

--- Main.py
+++ Main.py
@@ -2,6 +2,74 @@ import Functions as f
 
 # Definir el diccionario conjuntos en el ámbito global
 conjuntos = {}
+conjunto_universal = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz")
+
+# Ingresa cada elemento a su correspondiente pila
+def operar_conjuntos(expresion):
+    operandos = []
+    operadores = []
+    
+    tokens = expresion.split()
+    
+    for token in tokens:
+        if token in conjuntos:
+            operandos.append(conjuntos[token])
+        elif token in {'∪', '∩', '-', 'Δ', '!'}:
+            while (operadores and operadores[-1] != '('):
+                aplicar_operacion(operadores, operandos)
+            operadores.append(token)
+        elif token == '(':
+            operadores.append(token)
+        elif token == ')':
+            while operadores[-1] != '(':
+                aplicar_operacion(operadores, operandos)
+            operadores.pop()
+    
+    while operadores:
+        aplicar_operacion(operadores, operandos)
+    
+    return operandos[-1]
+
+# Saca los conjuntos de la pila y los opera
+def aplicar_operacion(operadores, operandos):
+    operador = operadores.pop()
+    if operador == '!':
+        conjunto = operandos.pop()
+        resultado = f.complemento(conjunto, conjunto_universal)
+    else:
+        conjunto2 = operandos.pop()
+        conjunto1 = operandos.pop()
+        if operador == '∪':
+            resultado = f.union(conjunto1, conjunto2)
+        elif operador == '∩':
+            resultado = f.interseccion(conjunto1, conjunto2)
+        elif operador == '-':
+            resultado = f.diferencia(conjunto1, conjunto2)
+        elif operador == 'Δ':
+            resultado = f.diferencia_simetrica(conjunto1, conjunto2)
+    operandos.append(resultado)
+
+# Devuelve al usuario el resultado de las operaciones
+def construir_y_operar_conjuntos():
+    flag =  True
+    while flag:
+    
+        if len(conjuntos) < 2:
+            print("\nSe necesitan al menos dos conjuntos para realizar una operación.")
+            return
+        
+        print("\nOperadores: ∪ (Union), ∩ (Interseccion), - (Diferencia), Δ (Diferencia simetrica), ! (Complemento)")
+        ver_conjuntos()
+        
+        expresion = input("Ingrese los identificadores de conjuntos, operadores y parentesis separados por espacios: ")
+        
+        try:
+            resultado = operar_conjuntos(expresion)
+            print(f"\n'{expresion}' = {resultado}")
+        except Exception as e:
+            print(f"Error al procesar la operación: {e}")
+        
+        return
 
 def ver_conjuntos():
     #Muestra todos los conjuntos creados y sus elementos.
@@ -51,7 +119,7 @@ def menu_principal():
             construir_conjunto()
 
         elif opcion == "2":
-            print("Operar conjuntos")
+            construir_y_operar_conjuntos()
 
         elif opcion == "3":
             ver_conjuntos()
@@ -65,3 +133,5 @@ def menu_principal():
 
 # Ejecutar el menú principal
 menu_principal()
+
+# Para utilizar complemento: ! A
